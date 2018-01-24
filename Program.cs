065dd8294bfa9e6f595cbb637cb6462ac18c5786
namnguyen0706 <namk45thue@gmail.com>
2018-01-24 14:03:25 +0700

    update

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilterEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string line ="";
+                Regex pattern1 = new Regex(@"(.+)(@+)(gmail+)(\.+)(com)");
+                // doc va ghi file
+                StreamReader sr = new StreamReader("test.txt");
+                StreamWriter sw = new StreamWriter("mail.txt");
+                
+                List<string> result = new List<string>();
+                while(line!=null)
+                {
+                    result.Add(line);
+                    line = sr.ReadLine();
+                }
+                foreach (var item in result)
+                {
+                    Match match = pattern1.Match(item);
+                    if(match.Success)
+                    sw.WriteLine(match);
+                }
+                }
+                catch (Exception ex)
+                {
+                
+                    System.Console.WriteLine(ex.Message);
+                }
+            
+        
+            
+        }
+    }
+}
